7ee4ee48bec83dbc425f75cfd86037b51b89332f
pabsan-0 <psantana@catec.aero>
2023-06-19 12:06:40 +0200

    common launch updated after new source example

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,7 @@
 echo 'yaml file:///catkin_ws/src/my_utils_pkg/rosdep.yaml' >> /etc/ros/rosdep/sources.list.d/20-default.list
 echo 'yaml file:///catkin_ws/src/my_js_pkg/rosdep.yaml' >> /etc/ros/rosdep/sources.list.d/20-default.list
 echo 'yaml file:///catkin_ws/src/my_python_pkg/rosdep.yaml' >> /etc/ros/rosdep/sources.list.d/20-default.list
+echo 'yaml file:///catkin_ws/src/my_source_pkg/rosdep/rosdep.yaml' >> /etc/ros/rosdep/sources.list.d/20-default.list
 rosdep update
 apt update
 rosdep install --from-paths /catkin_ws/src/ -y
